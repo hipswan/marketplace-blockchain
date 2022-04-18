27f60559ee0c1570b3828158d72fa46f15e06794
hipswan <atulsingh158@gmail.com>
2022-04-17 23:23:33 -0400

    commit final

--- foodex-contract/truffle-config.js
+++ foodex-contract/truffle-config.js
@@ -1,6 +1,3 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const MNEMONIC = "accuse genre bar crouch orchard wild tent youth breeze salon near weird";
-//ropsten.infura.io/v3/d51b8ae11bb34cdf9ecc3fc4b65cea07
 module.exports = {
   networks: {
     development: {
@@ -8,16 +5,7 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
     },
-    ropsten: {
-      provider: function () {
-        return new HDWalletProvider(
-          MNEMONIC,
-          "https://ropsten.infura.io/v3/d51b8ae11bb34cdf9ecc3fc4b65cea07"
-        );
-      },
-      network_id: 3,
-      gas: 4000000, //make sure this gas allocation isn't over 4M, which is the max
-    },
+   
   },
   contracts_directory: "./contracts/",
   contracts_build_directory: "../foodex-app/src/abis/",
