4fc03471928bdf54d4b92d19d26ac9d4570c36d3
hipswan <atulsingh158@gmail.com>
2022-04-20 01:19:15 -0400

    truffle hd wallet

--- foodex-contract/truffle-config.js
+++ foodex-contract/truffle-config.js
@@ -1,4 +1,6 @@
-
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require("dotenv").config();
+const MNEMONIC = process.env.MNEMONIC;
 module.exports = {
   networks: {
     development: {
@@ -6,7 +8,18 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
     },
-    
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: MNEMONIC,
+          },
+          providerOrUrl:
+            process.env.INFURA_ROPSTEN_URL,
+        }),
+      network_id: 3,
+      gas: 4000000, //make sure this gas allocation isn't over 4M, which is the max
+    },
   },
   contracts_directory: "./contracts/",
   contracts_build_directory: "../foodex-app/src/abis/",
